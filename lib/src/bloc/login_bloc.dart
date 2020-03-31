7536a4d9c3af277ffe406e398229f1abd11f56fd
muckystack <muckystack@gmail.com>
2020-03-30 20:53:15 -0600

    FEAT: Implemento los BehaviorSubject en login_bloc.dart

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,19 +1,27 @@
 import 'dart:async';
 
 import 'package:fluttervalidarformulariofh/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginBloc with Validators {
 
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   // Recuperar los datos del Stream
   Stream<String> get emailStream => _emailController.stream.transform(validarEmail);
   Stream<String> get passwordStream => _passwordController.stream.transform(validarPassword);
 
+  Stream<bool> get formValidStream =>
+    CombineLatestStream.combine2(emailStream, passwordStream, (e, p) => true);
+
   // Insertar valores al Stream
   Function(String) get changeEmail => _emailController.sink.add; // La estamos mandando como referencia
   Function(String) get changePassword => _passwordController.sink.add; // La estamos mandando como referencia
+
+  // Obtener el ultimo valor ingresado a los streams
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
 
   dispose() {
 
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -56,7 +56,7 @@ class LoginPage extends StatelessWidget {
                 SizedBox(height: 30.0),
                 _crearPassword(bloc),
                 SizedBox(height: 30.0),
-                _crearBoton()
+                _crearBoton(bloc)
               ],
             ),
           ),
@@ -120,21 +120,40 @@ class LoginPage extends StatelessWidget {
   }
 
 
-  Widget _crearBoton() {
+  Widget _crearBoton(LoginBloc bloc) {
 
-    return RaisedButton(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-        child: Text('Ingresar'),
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5.0)
-      ),
-      elevation: 0.0,
-      color: Colors.deepPurple,
-      textColor: Colors.white,
-      onPressed: () {},
+    // formValidStream
+
+    return StreamBuilder(
+      stream: bloc.formValidStream ,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        return RaisedButton(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Ingresar'),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0)
+          ),
+          elevation: 0.0,
+          color: Colors.deepPurple,
+          textColor: Colors.white,
+          onPressed: snapshot.hasData ? () => _login(bloc, context) : null,
+        );
+      },
     );
+  }
+
+  _login(LoginBloc bloc, BuildContext context) {
+
+    print('================');
+    print('Email: ${bloc.email}');
+    print('Password: ${bloc.password}');
+    print('================');
+
+
+    Navigator.pushReplacementNamed(context, 'home');
+
   }
 
 
